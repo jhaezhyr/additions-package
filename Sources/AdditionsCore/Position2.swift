cd97ed81213de7f89d04c1d98d8339ba411cbb59
Braeden Hintze <jhaezhyr12@gmail.com>
2023-11-22 02:34:23 -0700

    Refactor Position2

--- Sources/AdditionsCore/Position2.swift
+++ Sources/AdditionsCore/Position2.swift
@@ -9,44 +9,53 @@
 
 public struct Position2
 {
-	/// `true` if max, `false` if min, `nil` if this grip doesn't affect
-	public var x: Bool?
-	public var y: Bool?
-	
-	public static var minX: Position2 { return Position2(x: false, y: nil) }
-	public static var minY: Position2 { return Position2(x: nil, y: false) }
-	
-	public static var maxX: Position2 { return Position2(x: true, y: nil) }
-	public static var maxY: Position2 { return Position2(x: nil, y: true) }
-	
-	public static var minXminY: Position2 { return Position2(x: false, y: false) }
-	public static var minXmaxY: Position2 { return Position2(x: false, y: true) }
-	public static var maxXminY: Position2 { return Position2(x: true, y: false) }
-	public static var maxXmaxY: Position2 { return Position2(x: true, y: true) }
-	
-	public var rawValue: (Bool?, Bool?)
-		{ return (x, y) }
-	
-	public init(x: Bool?, y: Bool?)
-	{
-		self.x = x
-		self.y = y
-	}
-	
-	public init?(rawValue: (Bool?, Bool?))
-	{
-		self.x = rawValue.0
-		self.y = rawValue.1
-	}
-	
-	public var hashValue: Int
-		{ return (x?.hashValue ?? 0) ^ (y?.hashValue ?? 0) }
-	
-	public static func == (lh: Position2, rh: Position2) -> Bool
-		{ return lh.x == rh.x && lh.y == rh.y }
-	
-	public var inverted: Position2
-		{ return Position2(x: x.map(!), y: y.map(!)) }
+    /// `true` if max, `false` if min, `nil` if this grip doesn't affect
+    public var x: Bool?
+    public var y: Bool?
+    
+    public init(x: Bool? = nil, y: Bool? = nil)
+    {
+        self.x = x
+        self.y = y
+    }
 }
 
+extension Position2: Hashable, Equatable, Codable {}
 
+extension Position2: RawRepresentable
+{
+    public var rawValue: (Bool?, Bool?)
+        { return (x, y) }
+    
+    public init?(rawValue: (Bool?, Bool?))
+    {
+        self.x = rawValue.0
+        self.y = rawValue.1
+    }
+}
+
+extension Position2
+{
+    public static let center = Position2(x: nil, y: nil)
+    
+    public static let minX = Position2(x: false, y: nil)
+    public static let minY = Position2(x: nil, y: false)
+    
+    public static let maxX = Position2(x: true, y: nil)
+    public static let maxY = Position2(x: nil, y: true)
+    
+    public static let minXminY = Position2(x: false, y: false)
+    public static let minXmaxY = Position2(x: false, y: true)
+    public static let maxXminY = Position2(x: true, y: false)
+    public static let maxXmaxY = Position2(x: true, y: true)
+    
+    public static let allSides = [Position2.minX, .minY, .maxX, .maxY]
+    public static let allCorners = [Position2.minXminY, .minXmaxY, .maxXminY, .maxXmaxY]
+    public static let allValues = [Position2.center] + allSides + allCorners
+}
+
+extension Position2
+{
+    public var inverted: Position2
+        { return Position2(x: x.map(!), y: y.map(!)) }
+}
